66042d93f2f32cc057250a8b63801982e41950c3
markfungsc <markfungsc@outlook.com>
2021-08-20 14:43:46 +0800

    project rewrite

---
+++ src/instruction.rs
@@ -0,0 +1,41 @@
+use std::convert::TryInto;
+use solana_program::program_error::ProgramError;
+
+use crate::error::EscrowError::InvalidInstruction;
+
+pub enum EscrowInstruction {
+    InitEscrow {
+        // The amount party A expects to receive of token Y
+        amount: u64
+    },
+
+    Exchange {
+        // The amount the taker expected to pay 
+        amount: u64
+    }
+}
+
+impl EscrowInstruction {
+    pub fn unpack(input: &[u8]) -> Result<Self, ProgramError> {
+        let (tag, rest) = input.split_first().ok_or(InvalidInstruction)?;
+
+        Ok(match tag {
+            0 => Self::InitEscrow {
+                amount: Self::unpack_amount(rest)?,
+            },
+            1 => Self::Exchange {
+                amount: Self::unpack_amount(rest)?,
+            },
+            _ => return Err(InvalidInstruction.into()),
+        })
+    }
+
+    fn unpack_amount(input: &[u8]) -> Result<u64, ProgramError> {
+        let amount = input
+            .get(..8)
+            .and_then(|slice| slice.try_into().ok())
+            .map(u64::from_le_bytes)
+            .ok_or(InvalidInstruction)?;
+        Ok(amount)
+    }
+}
